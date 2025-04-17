fb45e089a4b51ae13fdb86bad52b6dc2b5884ccf
Pallavi Nile <nile.p@northeastern.edu>
2025-04-16 20:36:26 -0400

    ConnectFourMain.java file updated

--- src/main/java/com/phasmidsoftware/dsaipg/projects/mcts/connectFour/ConnectFourMain.java
+++ src/main/java/com/phasmidsoftware/dsaipg/projects/mcts/connectFour/ConnectFourMain.java
@@ -1,5 +1,25 @@
-package com.phasmidsoftware.dsaipg.projects.mcts.connectFour;
+/*
+ * Copyright (c) 2024. Robin Hillyard
+ */
 
-public class ConnectFourMain {
-    
-}
+ package com.phasmidsoftware.dsaipg.projects.mcts.connectfour;
+
+//  import com.phasmidsoftware.dsaipg.projects.mcts.connectfour;
+ 
+ import javax.swing.SwingUtilities;
+
+// import com.phasmidsoftware.dsaipg.projects.mcts.connectfour;
+ 
+ /**
+  * Main class to start the Connect Four game.
+  */
+ public class ConnectFourMain {
+     /**
+      * Main method to start the application.
+      * 
+      * @param args command line arguments.
+      */
+     public static void main(String[] args) {
+         SwingUtilities.invokeLater(ConnectFourUI::new);
+     }
+ }
